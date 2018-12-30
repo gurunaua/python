d6fbfaaa79188d14bb55d9f1b0b839c8ed19e2ad
aan <aan@localhost.localdomain>
2018-12-30 14:33:44 +0700

    belajar pemula

---
+++ belajar_input.py
@@ -0,0 +1,14 @@
+
+
+##coba lagi
+a = input("masukkan nilai a :")
+b = input("masukkan nilai a :")
+s = a + b
+print(f"hasil {a} + {b} = {s}")
+
+a = int(a)
+b= int(b)
+s = a + b
+print("*ini hasil ug di ubah jadi int")
+print(f"hasil {a} + {b} = {s}")
+
---
+++ conditional.py
@@ -0,0 +1,10 @@
+
+
+a =100
+
+if a<100 :
+    print("a kecil dari 100")
+    print("+++++++++++++1")
+else:
+    print("besar sama dari 100")
+    print("+++++++++++++2")
