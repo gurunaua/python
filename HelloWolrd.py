2d8547a9247296cdff45f743fc78f7a63db2b577
aan <aan@localhost.localdomain>
2018-12-30 09:21:15 +0700

    belajar string

---
+++ HelloWolrd.py
@@ -0,0 +1,10 @@
+print("Hello World")
+
+
+#coba print string
+nama="gurun aua"
+tulis_name1 = "tulis nama1 >> "+nama
+tulis_name2 = f"tulis nama1 >> {nama} " #cara alternative
+
+print(tulis_name1)
+print(tulis_name2)
